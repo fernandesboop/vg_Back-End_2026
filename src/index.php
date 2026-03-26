60e4810faf62d429b66d9beede21e2f0b7c47f81
fernandesboop <g.fernandes.santos@uni9.edu.br>
2026-03-26 14:13:01 +0000

    topico04.1

--- src/index.php
+++
@@ -1,5 +0,0 @@
-<h1>Back-End</h1>
-
-<?php
-
-phpinfo();
---
+++ src/pg01_1.php
@@ -0,0 +1,11 @@
+<?php
+    $idade=25;
+    $nota=8.5;
+    $nome="Bete";
+
+echo $nome, ", você tem ", $idade, " anos e a sua nota foi ", $nota;
+echo "<hr>";
+echo "$nome, você tem <b>$idade</b> anos e a sua nota foi $nota";
+
+?>
+
